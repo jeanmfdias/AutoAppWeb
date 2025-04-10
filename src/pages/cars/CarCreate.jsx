b3a2d4a3ca0351d918227c33fe3aa146a43f29dd
jeanmfdias <jeanmfdias@gmail.com>
2025-04-10 19:32:25 -0300

    feat: create a car

--- src/pages/cars/CarCreate.jsx
+++ src/pages/cars/CarCreate.jsx
@@ -1,7 +1,84 @@
+import {useState} from "react";
+import {http} from "../../config/http.jsx";
+import {saveCar} from "../../services/cars.service.jsx";
+
 export default function CarCreate() {
+  const [brand, setBrand] = useState("");
+  const [model, setModel] = useState("");
+  const [factoryYear, setFactoryYear] = useState("");
+  const [modelYear, setModelYear] = useState("");
+  const [acquiredPrice, setAcquiredPrice] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const response = await saveCar({
+      brand: brand,
+      model: model,
+      factory_year: factoryYear,
+      model_year: modelYear,
+      acquired_price: acquiredPrice,
+    });
+
+    if (response.status === 200) {
+      setBrand("");
+      setModel("");
+      setFactoryYear("");
+      setModelYear("");
+      setAcquiredPrice("");
+      setSuccess(true);
+    }
+  }
+
   return (
     <>
-     <h1>Add New Car</h1>
+      <h1>Add New Car</h1>
+
+      {success ? (<div className="alert alert-success" role="alert">Car saved successfully!</div>) : null}
+
+      <form className="form">
+        <div className="form-group mb-3">
+          <input type="text"
+                 placeholder="Brand"
+                 className="form-control"
+                 id="brand"
+                 value={brand}
+                 onChange={(e) => setBrand(e.target.value)} />
+        </div>
+        <div className="form-group mb-3">
+          <input type="text"
+                 placeholder="Model"
+                 className="form-control"
+                 id="model"
+                 value={model}
+                 onChange={(e) => setModel(e.target.value)} />
+        </div>
+        <div className="form-group mb-3">
+          <input type="number"
+                 placeholder="Factory Year"
+                 className="form-control"
+                 id="factoryYear"
+                 value={factoryYear}
+                 onChange={(e) => setFactoryYear(e.target.value)}/>
+        </div>
+        <div className="form-group mb-3">
+          <input type="number"
+                 placeholder="Model Year"
+                 className="form-control"
+                 id="modelYear"
+                 value={modelYear}
+                 onChange={(e) => setModelYear(e.target.value)}/>
+        </div>
+        <div className="form-group mb-3">
+          <input type="number"
+                 placeholder="Acquired Price"
+                 className="form-control"
+                 id="acquiredPrice"
+                 value={acquiredPrice}
+                 onChange={(e) => setAcquiredPrice(e.target.value)}/>
+        </div>
+        <button onClick={(e) => submitHandler(e)} className="btn btn-success" type="button">Save</button>
+      </form>
     </>
   );
 }
--- src/services/cars.service.jsx
+++ src/services/cars.service.jsx
@@ -3,3 +3,7 @@ import {http} from "../config/http.jsx";
 export function getAll() {
   return http.get("/vehicles");
 }
+
+export function saveCar(vehicle) {
+  return http.post("/vehicles", vehicle);
+}
